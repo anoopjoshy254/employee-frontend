8623941d163d205c53e03298c5e31a268af0441c
Anoopjoshy254 <anoopjoshy254@gmail.com>
2026-06-03 16:22:11 +0530

    login signup done

---
+++ src/app/component/register/register.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  registerForm: FormGroup;
+  submitted = false;
+  loading = false;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.registerForm = this.fb.group(
+      {
+        fullName: ['', [Validators.required, Validators.minLength(2)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required]
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    setTimeout(() => {
+      this.router.navigate(['/home']);
+      this.loading = false;
+    }, 1000);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+}
